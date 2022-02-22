33d21603e99130c00bc27a42ed22d0378fd32c32
Ozan <ozan28061@gmail.com>
2022-02-23 00:08:45 +0100

    operators explained

---
+++ operators.js
@@ -0,0 +1,71 @@
+// Operators
+
+// let value ;
+// const a = 50;
+// const b = 10; 
+// let c = 12;
+
+// 1 - Arithmetic Operators
+
+    // value = a + b
+    // value = a - b
+    // value = a * b
+    // value = a / b
+    // value = b % a
+    // value = c++; // 12
+    // value = ++c // 14
+    // value = c-- //13
+    // value = --c// 11
+
+// ***********************************
+
+ // 2 - Assign operators
+
+    // value = a;
+    // value += a; value = value + a
+    // value -= a; value = value - a
+    // value *= a; value = value * a
+    // value /= a; value = value / a
+    // value %= a; value = value % a
+
+// ****************************************
+
+ // 3 - Comparing Operators
+
+    // const d = 5
+    // const e = "5"
+    // const f = 5
+    // value = a == b false
+    // value = d == e true
+    // value = d === e false because one of them is string 
+    // value = d === f true
+    // value = a != b
+    // value = b >= a
+    // value = b > a
+
+
+// *********************************************
+
+
+// 4 - Logical operators
+
+    // && (And)
+
+    // value = (a>b) && (a<b) false
+    // value = (a>b) && (a>c) true
+    
+    // || (Or)
+
+    // value = (a>b) || (b>a) true 
+    // value = (a>b) || (b>a) if one of them is true result will be true.
+
+
+    // ! (Not)
+
+    // value = !true
+    // value = !false
+
+
+//  console.log(value)
+
+
