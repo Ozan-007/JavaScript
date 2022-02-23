7edfe4a7a68613bc8b4b74199efdf978712f8a8c
Ozan <ozan28061@gmail.com>
2022-02-23 15:22:48 +0100

    Numbers and Math methods explained

---
+++ Numbers.js
@@ -0,0 +1,74 @@
+// let val;
+// let num = 10.1234
+
+// val = num.toPrecision(3)
+// val = num.toFixed(3)
+
+// val = Math.PI
+// val = Math.cos(0)
+// val = Math.round(num)
+// val = Math.ceil(2.3)
+// val = Math.floor(2.9)
+// val = Math.sqrt(256)
+// val = Math.pow(2,5)
+// val = Math.abs(-78)
+// val = Math.max(1,23,4,5,234,46)
+// val = Math.min(1,23,4,5,234,46)
+// val = Math.random()
+// val = Math.floor(Math.random(1,10)*10)
+
+// console.log(val)
+
+
+// let number = 16.23466452;
+
+// Get only 3 digit of the number 
+
+    // number = number.toPrecision(3)
+    // number = number.toFixed(1)
+    
+
+// Limit fixed points with 3 digits.
+
+    // number = number.toPrecision(5)
+    // number = number.toFixed(3)
+
+
+// Round it to the closest number
+
+    // number = Math.round(number)
+    
+// Round it to floor.
+
+        // number = Math.floor(number)
+        
+// Round it to ceil.
+                
+        // number = Math.ceil(number)
+
+
+//Find the smallest and the biggest between 1,2,10,57,43
+
+        // console.log(Math.min(1,2,10,57,43))
+        // console.log(Math.max(1,2,10,57,43))
+        
+
+// Create random numbers
+
+        // console.log(Math.floor(Math.random(1,100)*10))
+
+
+// ***********************************************
+// Calculate salary
+
+//Gross salary: 3700
+
+//Extra work hours: 10.3
+
+// Tax pay %25 , If a worker works for 42 extra hours What is the workers net salary?
+
+// let extra  = 42*10.3
+// let gross = 3700
+// let total_gross = gross + extra
+// let net_salary = total_gross - total_gross*25/100
+// console.log(Math.floor(net_salary))
