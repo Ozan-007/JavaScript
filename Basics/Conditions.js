37b9475af2985871bea80b3b22b84655a917d5e8
Ozan <ozan28061@gmail.com>
2022-02-25 14:41:45 +0100

    If else and switch case explained

---
+++ Basics/Conditions.js
@@ -0,0 +1,103 @@
+// // IF ELSE
+
+
+// const fullName="Ozan Eski";
+// const age=19;
+// const isAdmin = true;
+// const university = "Cambridge";
+
+// if(fullName == "Ozan Eski"){
+//     console.log('Hi Ozan')
+// }
+// else{
+//     console.log("who are you?")
+// }
+
+// if (age>=19){
+//     console.log("You can have license")
+// }
+// else {
+//     console.log('you can not have license')
+// }
+
+
+
+// if (age>0 && age<12){
+//     console.log("This is a child")
+// }
+// else if(age>13 && age<18){
+//     console.log("This is a teenager")
+// }  
+// else{
+//     console.log("This is an adult")
+// }
+
+
+// *********************************************
+
+// Switch 
+
+// let new_day;
+// console.log(new Date().getDay())
+
+// switch(new Date().getDay()){
+
+    
+//     case 1:
+        
+//     case 2:
+            
+//     case 3:
+                
+//     case 4:
+    
+//     case 5:
+//         new_day="Week day"
+//         break
+                        
+//     case 0:
+//     case 6:
+//         new_day = 'Weekend'
+//     break
+// }
+//     case 0:
+//         new_day = 'Sunday'
+//     break
+
+//     case 1:
+//         new_day = 'Monday'
+//     break
+
+//     case 2:
+//         new_day = 'Tuesday'
+//     break
+
+//     case 3:
+//         new_day = 'Wednesday'
+//     break
+
+//     case 4:
+//         new_day = 'Thursday'
+//     break
+
+//     case 5:
+//         new_day = 'Friday'
+//     break
+
+//     case 6:
+//         new_day = 'Saturday'
+//     break
+// }
+
+console.log(new_day)
+
+
+
+
+
+
+
+
+
+
+
