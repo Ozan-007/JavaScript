78683431a15fd6e2fd2c9c0c865fb79cc46ca585
Ozan <ozan28061@gmail.com>
2022-02-27 16:24:07 +0100

    multiple element selection explained

--- Dom/Dom.js
+++ Dom/Dom.js
@@ -30,15 +30,53 @@
 // document.querySelector("#header")
 // document.querySelector(".header") // import part while using querselecyor is we need to use # and . for representing classes and ids.
 
-console.log(document.querySelector('li').style.color = "red")
-console.log(document.querySelector('li:last-child').style.color = "blue")
-console.log(document.querySelector('li:nth-child(2)').style.color = "blue")
+// console.log(document.querySelector('li').style.color = "red")
+// console.log(document.querySelector('li:last-child').style.color = "blue")
+// console.log(document.querySelector('li:nth-child(2)').style.color = "blue")
 
-document.querySelector('li:nth-child(2)').textContent = 'Task Item'
-
-
+// document.querySelector('li:nth-child(2)').textContent = 'Task Item'
 
 
 
-// ** 
+// **************************************************************
+
+// ** Multiple Elements Selection 
+
+let val;
+// val = document.getElementsByClassName('element')
+// console.log(val[0])
+
+// for ( let i=0; i<val.length; i++ ){
+
+//     val[i].style.color = "blue"
+//     val[i].style.fontWeight = "bold"
+//     val[i].textContent = "New element"
+
+// }
+
+// **********
+
+// val = document.getElementsByTagName('li')
+// console.log(val)
+// val[0].style.color = "blue"
+
+
+// **********
+
+// val = document.querySelectorAll('li');
+
+// val = document.querySelectorAll('li:nth-child(odd)');
+
+// val.forEach(function(item) {
+//     item.style.background = "blue"
+// })
+// val.forEach((item) => {
+//     item.style.background = "blue"
+// })
+
+// val[0].style.color = "red"
+
+
+
+
 
