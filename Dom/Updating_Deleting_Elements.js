972ef92a869b2758f8947372aac8d456c979a657
Ozan <ozan28061@gmail.com>
2022-03-01 17:48:10 +0100

    Removing,Replacing and Creating element explained.

---
+++ Dom/Updating_Deleting_Elements.js
@@ -0,0 +1,62 @@
+
+const list = document.querySelector('ul');
+
+// ** Removing 
+
+// list.childNodes[3].remove()
+
+// list.children[2].remove();
+
+// list.removeChild(list.children[0]);
+
+// console.log(list.childNodes[3])
+
+// console.log(list.children[0])
+
+
+// ***** Remove attribute
+
+// list.removeAttribute('class')
+// list.children[0].removeAttribute('class')
+
+
+// ** Replacing ELements
+
+// const header = document.querySelector('#header')
+
+// console.log(header)
+// const new_header = document.createElement('header')
+
+// new_header.setAttribute('class','new-header')
+
+// new_header.appendChild(document.createTextNode("New To-do App"))
+
+// const todo = document.querySelector('.todo')
+
+// todo.appendChild(new_header)
+
+// todo.replaceChild(new_header,header)
+
+
+
+// ** Create
+
+// const new_li = document.createElement('li')
+
+// new_li.appendChild(document.createTextNode("Fourth One"))
+
+// new_li.setAttribute('class','element')
+
+// const ul = document.querySelector('.list-group')
+
+// ul.appendChild(new_li)
+
+
+
+
+
+
+
+
+
+
