275221268540ca2002e061ac2d2aa84f06e5a165
Ozan <ozan28061@gmail.com>
2022-03-02 20:36:31 +0100

    Event Listeners explained.

---
+++ Dom/Event_Listeners.js
@@ -0,0 +1,18 @@
+// Event Listeners
+
+// const btn = document.querySelector('.new');
+
+// btn.addEventListener('click', function(){
+//     console.log('btn clicked')
+// })
+
+// function btnClick(e){
+//     console.log('btn clicked');
+//     console.log(e.target);
+//     console.log(e.target.classList);
+//     console.log(e.type);
+// }
+
+// const deleteButton = document.querySelector('.delete')
+
+// deleteButton.addEventListener('click', btnClick)
