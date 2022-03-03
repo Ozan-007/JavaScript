2c2462d599820ded4e40a2f78f6d96cd9af82c4b
Ozan <ozan28061@gmail.com>
2022-03-03 19:09:48 +0100

    Mouse event explained

---
+++ Dom/Mouse_Events.js
@@ -0,0 +1,48 @@
+// Mouse Events
+
+
+//function
+// let eventHandler = (e) => {
+//     console.log(e.type)
+//     h2.textContent=`X : ${e.offsetX} , Y : ${e.offsetY}`;
+
+// }
+// // click
+// let ul = document.querySelector('.list-group')
+
+// ul.addEventListener('click', eventHandler)
+//double click
+
+// ul.addEventListener('dblclick', eventHandler)
+
+
+// mouse down
+// ul.addEventListener('mousedown',eventHandler )
+
+// mouse up
+// ul.addEventListener('mouseup',eventHandler )
+
+//mouse enter
+// ul.addEventListener('mouseenter',eventHandler )
+
+
+// mouse leave
+// ul.addEventListener('mouseleave',eventHandler )
+
+// mouse over
+// ul.addEventListener('mouseover',eventHandler )
+
+// mouse out
+// ul.addEventListener('mouseover',eventHandler )
+
+
+// mouse  move
+ul.addEventListener('mousemove',eventHandler )
+
+
+
+//offsety, offsetx
+
+let h2 = document.querySelector('h2')
+
+
