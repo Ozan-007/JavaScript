52b07740e81cee6040ff2d550851e2b569d6dac5
Ozan <ozan28061@gmail.com>
2022-03-27 23:57:08 +0200

    Prototypal Inheritance

--- OOP/script.js
+++ OOP/script.js
@@ -1,16 +1,57 @@
-let person = {
-    job: "student",
-    getAge: function(yearofBirth){
-        return new Date().getFullYear() - this.yearofBirth;
-    }
-}
+// let person = {
+//     job: "student",
+//     getAge: function(yearofBirth){
+//         return new Date().getFullYear() - this.yearofBirth;
+//     }
+// }
 
-let ozan = Object.create(person) // We can define it like this also we can add values of the object as the second property.
-let harry = Object.create(person,{
-    name:{value:"Harry"},
-    yearofBirth:{value:1989}
-})
+// let ozan = Object.create(person) // We can define it like this also we can add values of the object as the second property.
+// let harry = Object.create(person,{
+//     name:{value:"Harry"},
+//     yearofBirth:{value:1989}
+// })
 
 // ozan.yearofBirth = 2000
 // console.log(ozan) // We can take an object attributes to another object as prototype.
-console.log(harry)
+// console.log(harry)
+
+// ****************************************
+
+// Prototypal Inheritance
+
+let Person = function(name,yearofBirth,job){
+
+    this.name = name;
+    this.yearofBirth = yearofBirth;
+    this.job = job;
+}
+Person.prototype.calculateAge = function(){
+   return 2022 - this.yearofBirth;
+}
+
+
+let Teacher = function(name,yearofBirth,job,subject){
+    Person.call(this, name,yearofBirth,job)
+    this.subject = subject;
+    
+}
+
+// Inherit the Person prototype methods.
+Teacher.prototype = Object.create(Person.prototype)
+
+// console.log(Teacher.prototype.constructor)
+
+Teacher.prototype.constructor = Teacher;
+
+
+let james = new Teacher("James",1996,"teacher","math")
+console.log(james.calculateAge())
+
+
+
+
+
+
+
+
+
