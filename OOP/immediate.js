ccd2ad89900258b9da20fbb293c1542e158aacbd
Ozan <ozan28061@gmail.com>
2022-04-02 17:51:24 +0200

    added Immediate functions.

---
+++ OOP/immediate.js
@@ -0,0 +1,28 @@
+//IMMEDIATE FUNCTIONS
+
+
+// function welcome(name) {
+    
+//     let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday",'Friday',"Saturday"];
+    
+//     let date = new Date().getDay();
+    
+//     console.log(`Welcome ${name} today is ${days[date]}.`)
+// }
+
+// welcome("Ozan");
+
+//!! if we want to use a function and we want it to work just one time, We don't have to waste time on naming it and calling it etc. We can simply use ↓↓↓ 
+
+// Immediate
+
+
+// (function(name) {
+    
+//     let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday",'Friday',"Saturday"];
+    
+//     let date = new Date().getDay();
+
+//     console.log(`Hello ${name} today is ${days[date]}`)
+
+// }("James")) // This is why they are called immediate functions...
