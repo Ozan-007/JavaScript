505dc505e9affb534e9c7ebc172d42253eb9d782
Ozan <ozan28061@gmail.com>
2022-04-08 19:14:49 +0200

    Quiz object and protoype methods are created.

--- Projects/Quiz-App/script.js
+++ Projects/Quiz-App/script.js
@@ -1,20 +1,65 @@
 // Creating Question Objects.
 
-function Question(question,choices,answer) {
-    this.question = question;
-    this.choices = choices;
-    this.answer = answer;
+function Question(question, choices, answer) {
+  this.question = question;
+  this.choices = choices;
+  this.answer = answer;
 }
 
 // QUestion prototype
-Question.prototype.checkAnswer = function(answer){
-    return this.answer == answer;
-}
+Question.prototype.checkAnswer = function (answer) {
+  return this.answer == answer;
+};
 
 let question1 = new Question(
-    "What is the best sport ?",
-    ["basketball","soccer","tennis"],
-    "basketball"
-    )
+  "What is the best sport ?",
+  ["basketball", "soccer", "tennis"],
+  "basketball"
+);
+let question2 = new Question(
+  "Which city is the capital of Turkey ?",
+  ["Ankara", "Istanbul", "Antalya"],
+  "Ankara"
+);
 
-console.log(question1.checkAnswer('basketball'))
+// console.log(question1.checkAnswer("basketball"));
+
+// Quiz Constructor
+
+let questions = [question1, question2];
+function Quiz(questions) {
+  this.questions = questions;
+  this.score = 0;
+  this.questionIndex = 0;
+}
+
+Quiz.prototype.getQuestion = function () {
+  console.log(this.questions[this.questionIndex].question);
+};
+
+Quiz.prototype.isFinished = function () {
+    return this.questions.length === this.questionIndex;
+};
+
+Quiz.prototype.guess = function(answer){
+    if (this.questions[this.questionIndex].answer.toLowerCase != answer.toLowerCase) {
+        console.log("Wrong.");
+    }
+    else{
+        console.log("Correct!!");
+    }
+    this.questionIndex++;
+        
+}
+
+let quiz = new Quiz(questions);
+
+quiz.getQuestion();
+quiz.guess("Basketball")
+console.log(quiz.questionIndex);
+quiz.getQuestion();
+quiz.guess("ankara")
+
+// console.log(quiz.isFinished());
+
+
